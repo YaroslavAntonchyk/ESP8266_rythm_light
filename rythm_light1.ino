efbc10e7a2e5d698e44cc5e54bdc5f5415741500
Yaroslav Antonchyk <yaroslav.antonchik@gmail.com>
2020-12-20 22:57:21 +0100

    raw but working one

--- rythm_light1.ino
+++ rythm_light1.ino
@@ -5,11 +5,14 @@
 #define LED_PIN     2
 #define NUM_LEDS    58
 #define SAMPLES     50
+#define MIN_LED_LEVEL 20
+#define MAX_LED_LEVEL 255
 
 CRGB leds[NUM_LEDS];
 uint16_t volume_level = 0;
 uint16_t volume_level_max = 0;
 uint16_t abs_volume_level = 0;
+uint8_t out_volume_level = 0;
 float volume_level_filtred = 500.0F;
 float mean_volume = 420;
 // for debuging 
@@ -38,12 +41,13 @@ void loop()
       volume_level_max = abs_volume_level;
   }
   // low pass filter
-  volume_level_filtred = static_cast<float>(volume_level_max)*0.05 + volume_level_filtred*0.95;
-
-  plot_level(static_cast<uint16_t>(volume_level_filtred));
+  volume_level_filtred = static_cast<float>(volume_level_max)*0.07 + volume_level_filtred*0.93;
+  out_volume_level = map(static_cast<uint16_t>(volume_level_filtred), 0, static_cast<uint16_t>(mean_volume), MIN_LED_LEVEL, MAX_LED_LEVEL);
+  out_volume_level = static_cast<uint8_t>(constrain(out_volume_level, MIN_LED_LEVEL, MAX_LED_LEVEL));
+  plot_level(static_cast<uint16_t>(out_volume_level));
 
   for(int k = 0; k < NUM_LEDS; k++)
-    leds[k] = CRGB(0, 128/4, 64/4); 
+    leds[k] = CRGB(0, out_volume_level, out_volume_level); 
   FastLED.show();
   // t = micros();
 
